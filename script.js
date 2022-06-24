fd09113658f336190a6988ec6d73bba81d7a4c7e
karthik11135 <105512128+karthik11135@users.noreply.github.com>
2022-06-24 14:48:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// ctx.fillRect()illRect('')
-ctx.fillStyle = "white";
-ctx.font = "2.4rem Arial";
-
-// ctx.fillText("Delta Task 2", canvas.width / 2 - 40, 40);
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const spikeFn = () => {
   ctx.fillStyle = "red";
@@ -20,35 +19,103 @@ const spikeFn = () => {
     ctx.fill();
   }
 };
-// spikeFn()
 
-const blocks = [];
-
-ctx.fillStyle = "skyblue";
-const blockFn = function (a, b, c = 90, d = 20) {
-  ctx.fillRect(a, b, c, d);
+const func = (param) => {
+  return {
+    x: randomIntFromInterval(0, 500),
+    y: randomIntFromInterval(70, 800),
+    width: 100,
+    height: 8,
+    dy: 1,
+  };
 };
 
-const randomFn = () => {
-  const numOfBlocksInEachLn = Math.trunc(Math.random() * 3) + 1;
-  const yDistance = Math.trunc(Math.random() * 600) + 1;
-  const xDistance = [
-    Math.trunc(Math.random() * 600) + 1,
-    Math.trunc(Math.random() * 600) + 1,
-    Math.trunc(Math.random() * 600) + 1,
-  ];
-  console.log(numOfBlocksInEachLn, yDistance, xDistance[0]);
-  blockFn(xDistance[0], yDistance);
-  blockFn(xDistance[1], yDistance);
-};
+console.log(func(8));
+
+// const generateBar = (bar) => {
+//   const bar = {
+//     x: Math.trunc(Math.random() * 600),
+//     y: Math.trunc(Math.random() * 600),
+//     width: 150,
+//     height: 10,
+//     dy: 1,
+//   };
+//   return bar;
+// };
+
+// console.log(generateBar());
 
 const circle = {
   x: 50,
   y: 50,
   size: 20,
   speed: 2,
+  speedY: 2,
   dx: 0,
   dy: 0,
+};
+
+const barXCo = randomIntFromInterval(70, 500);
+const barYCo = randomIntFromInterval(100, 600);
+
+const bar1 = {
+  x: barXCo,
+  y: barYCo,
+  width: 100,
+  height: 8,
+  dy: 1,
+};
+
+const bar2 = {
+  x: barXCo,
+  y: barYCo,
+  width: 100,
+  height: 8,
+  dy: 1,
+};
+
+const bar3 = {
+  x: randomIntFromInterval(0, 500),
+  y: randomIntFromInterval(400, 500),
+  width: 100,
+  height: 8,
+  dy: 1,
+};
+
+const bar4 = {
+  x: randomIntFromInterval(0, 500),
+  y: randomIntFromInterval(500, 600),
+  width: 100,
+  height: 8,
+  dy: 1,
+};
+
+const bars = [bar1, bar2, bar3, bar4];
+
+const drawBar = (barsArr) => {
+  barsArr.forEach((bar) => {
+    ctx.fillStyle = "cyan";
+    ctx.beginPath();
+    ctx.fillRect(bar.x, bar.y, bar.width, bar.height);
+    ctx.fill();
+  });
+};
+
+const holdBall = (barsArr) => {
+  barsArr.forEach((bar) => {
+    bar.y -= bar.dy;
+    if (
+      circle.y + circle.size >= bar.y &&
+      circle.x + circle.size / 2 >= bar.x &&
+      circle.x - circle.size / 2 < bar.x + bar.width &&
+      circle.y + circle.size < bar.y + bar.height
+    ) {
+      circle.speedY = 0;
+      circle.y = bar.y - circle.size;
+    } else {
+      circle.speedY = 2;
+    }
+  });
 };
 
 const drawCircle = () => {
@@ -58,32 +125,38 @@ const drawCircle = () => {
   ctx.fill();
 };
 
+let i = 0;
 const update = function () {
+  i++;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // randomFn();
   spikeFn();
   drawCircle();
-  ctx.fillStyle = 'blue'
-  ctx.fillRect(90, 300, 150, 10);
-  if(circle.x > 90 && circle.x < 300 && circle.y + circle.size === 300) {
-    // circle.speed = 0;
-    circle.dy = 0;
-    circle.y = 300 - circle.size;
-    console.log('ef')
-  }
-  circle.x += circle.dx;
-  circle.y += circle.speed;
+  drawBar(bars);
+  holdBall(bars);
 
-  if(circle.y > canvas.height) { 
-    circle.y = canvas.height;
+  if (i % 90 === 0) {
+    bars.push({
+      x: randomIntFromInterval(0, 500),
+      y: randomIntFromInterval(700, 800),
+      width: 100,
+      height: 8,
+      dy: 1,
+    });
+  }
+
+  circle.x += circle.dx;
+  circle.y += circle.speedY;
+
+  if (circle.y + circle.size >= canvas.height) {
+    circle.y = canvas.height - circle.size;
+  }
+
+  if (circle.y - circle.size === 20) {
+    alert("game over");
   }
 
   if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
     circle.x *= -1;
-  }
-
-  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
-    circle.dy *= -1;
   }
 
   requestAnimationFrame(update);
@@ -110,7 +183,6 @@ const keyDown = (e) => {
 const keyUp = (e) => {
   if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
     circle.dx = 0;
-    circle.dy = 0;
   }
 };
 
